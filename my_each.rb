21cd516967e79a542d611a30cbd30e9c0f02a3d9
Christian V <viray.christianj@gmail.com>
2018-10-12 19:44:53 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,11 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(words)
+  
+while i < words.length 
+  puts word 
+  counter += 1
+end 
+  words
+
+else 
+  puts "There is no collection"
 end
